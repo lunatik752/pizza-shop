19e6940576c429f7d95dc52ec13707fbf43f9974
knakna <kna@tut.by>
2024-10-10 21:55:34 +0300

    change global.css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import {Button} from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <h1>
-      Home
-    </h1>
-  );
+    return (
+        <>
+            <h1>
+                Home
+            </h1>
+            <Button variant={'outline'}>Корзина</Button>
+        </>
+    );
 }
