d97c0ff0262ecf81963d98faccc973bbf9ee2e53
Maksim Bely <whitemaxim752@gmail.com>
2024-10-15 15:57:34 +0300

    add sort popup component

---
+++ components/shared/SortPopup.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {cn} from "@/lib/utils";
+import {ArrowUpDown} from "lucide-react";
+
+type SortPopupPropsType = {
+    className?: 'string'
+}
+
+export const SortPopup: React.FC<SortPopupPropsType> = ({className}) => {
+    return (
+        <div className={cn('inline-flex items-center gap-1 bg-gray-50 px-5 h-[52px] rounded-2xl cursor-pointer', className)}>
+            <ArrowUpDown size={16}/>
+            <b>Сортировка</b>
+            <b className={'text-primary'}>популярное</b>
+        </div>
+    );
+};
+
+
